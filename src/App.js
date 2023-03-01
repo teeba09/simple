78094c45db703ac39e73c0244a9de400d7f2fa5b
Teeba alnaser <teeba.ali123@yahoo.com>
2023-03-01 16:59:33 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useQuery, useQueryClient } from "react-query";
-import { getPosts } from "./api";
+import { useQuery, useMutation, QueryClient } from "react-query";
+import { deletePosts, getPosts } from "./api";
 
 function App() {
-  const query = useQuery("posts", getPosts);
+  const { isLoading, error, data } = useQuery("posts", getPosts);
+  const queryClient = new QueryClient();
+
+  const mutation = useMutation(deletePosts, {
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries("posts");
+    },
+  });
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
 
   return (
-    <div className="App">
-      {query.data.map((e) => (
-        <h1> {e.title}</h1>
+    <div className=" border border-emerald-700  m-10">
+      {data?.map((e) => (
+        <div className="flex justify-between mx-2">
+          <div>
+            <h1 className=" my-2"> {e.title}</h1>
+          </div>
+          <div className="flex justify-between gap-3">
+            <button
+              className="text-red-500"
+              onClick={(p) => {
+                mutation.mutate({
+                  id: e.id,
+                });
+              }}
+            >
+              {" "}
+              delete
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
--- src/api/index.js
+++ src/api/index.js
@@ -6,3 +6,22 @@ export const getPosts = async () => {
     console.log("Error has occured :" + err);
   }
 };
+
+export const deletePosts = async (id) => {
+  console.log("id", id);
+  try {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return res.json();
+  } catch (err) {
+    console.log("Error has occured :" + err);
+  }
+};
